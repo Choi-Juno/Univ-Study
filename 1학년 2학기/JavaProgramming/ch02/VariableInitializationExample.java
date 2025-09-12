fad0aca9e1a2d411cac98c17225008a7d2b4166d
Juno <sojae0306@gmail.com>
2025-09-12 14:37:46 +0900

    2주 1교시

---
+++ 1학년 2학기/JavaProgramming/ch02/VariableInitializationExample.java
@@ -0,0 +1,12 @@
+package JavaProgramming.ch02;
+
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+        int iVal = 10;
+
+        int iResult = iVal + 10;
+
+        System.out.println("iResult = " + iResult);
+
+    }
+}
